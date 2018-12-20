50a66a262ec913e12fea6843f4c3932ad850c9f8
hashy0917 <oohasi3939@gmail.com>
2018-12-20 10:33:07 +0900

    edit urlopen_meta.py

--- src/urlopen_meta.py
+++ src/urlopen_meta.py
@@ -1,12 +1,22 @@
+import re
 import sys
 from urllib.request import urlopen
 
 f = urlopen("https://sample.scraping-book.com/dp")
+bytes_content = f.read() # bytes型のレスポンスボディを一旦変数に格納する
 
-# HTTPヘッダーからエンコーディングを取得する(明示されていない場合は"utf-8"とする)
-encoding = f.info().get_content_charset(failobj="utf-8")
+# charsetはHTMLの最初の方に書いてあるはずなので
+# レスポンスボディの先頭1024byteをASCII文字列としてdecodeする
+# ASCII範囲外の文字は`U+FFFD(REPLACEMENT CHARACTER)`に置き換え例外を発生させない
+scanned_text = bytes_content[:1024].decode('ascii',errors='replace')
 
-print("encoding:",encoding,file=sys.stderr)
+match = re.search(r'charset=["\']?([\w-]+)',scanned_text)
+if match:
+    encoding = match.group(1)
+else:
+    encoding = "utf-8"
 
-text = f.read().decode(encoding)
+print('encoding:',encoding,file=sys.stderr)
+
+text = bytes_content.decode(encoding)
 print(text)
